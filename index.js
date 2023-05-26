008f0a1f5a413a91ac13815109f978f65f8e68ae
kkahramann <kkaradavut.3@gmail.com>
2023-05-26 14:55:04 +0300

    swagger update

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
-import express, { json, urlencoded } from 'express';
-import { serve, setup } from 'swagger-ui-express';
-import swaggerJSDoc from 'swagger-jsdoc';
-import helmet from "helmet";
-import cors from "cors";
+const express = require('express');
+const { json, urlencoded } = require('express');
+const { serve, setup } = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+const helmet = require('helmet');
+const cors = require('cors');
 
-require("dotenv").config();
+const dotenv = require('dotenv');
+dotenv.config();
 
-var app = express();
+// ROUTES
+const userRoutes = require('./src/routes/user');
+const authRoutes = require('./src/routes/auth');
+const productRoutes = require('./src/routes/product');
+const transactionRoutes = require('./src/routes/transaction');
+
+const app = express();
 app.use(json());
 app.use(helmet());
-app.use(urlencoded({extended: true}));
-
-
+app.use(urlencoded({ extended: true }));
 
 // SWAGGER
 
@@ -32,37 +38,26 @@ const swaggerDefinition = {
 
 const options = {
   swaggerDefinition,
-  apis: ['./src/routes/*.js'], 
+  apis: ['./src/routes/*.js'],
 };
 const swaggerSpec = swaggerJSDoc(options);
 
 app.use('/api-docs', serve, setup(swaggerSpec));
 
-
-
-
-
-
-// ROUTES
-import userRoutes from "./src/routes/user";
-import authRoutes from "./src/routes/auth";
-import productRoutes from "./src/routes/product";
-import transactionRoutes from "./src/routes/transaction";
-
 const corsOptions = {
   credentials: true,
   origin: [
-    "http://localhost:3000",
-    "http://192.168.2.112:3000",
+    'http://localhost:3000',
+    'http://192.168.2.112:3000',
   ],
   allowedHeaders: [
-    "Content-Type",
-    "Authorization",
-    "Access-Control-Allow-Origin",
+    'Content-Type',
+    'Authorization',
+    'Access-Control-Allow-Origin',
   ],
   exposedHeaders: [
-    "Content-Range",
-    "X-Content-Range",
+    'Content-Range',
+    'X-Content-Range',
   ],
   maxAge: 600,
   preflightContinue: false,
@@ -71,22 +66,22 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // USER
-app.use("/api/auth", authRoutes);
-app.use("/api/users", userRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
 
 // PRODUCT
-app.use("/api/products", productRoutes);
+app.use('/api/products', productRoutes);
 
 // TRANSACTION
-app.use("/api/transactions", transactionRoutes);
+app.use('/api/transactions', transactionRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send({error: true, message: 'Route bulunamadı!', result: null})
+  res.status(404).send({ error: true, message: 'Route bulunamadı!', result: null });
   next();
-})
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, function () {
-  console.log("Started application on port %d", port);
+  console.log('Started application on port %d', port);
 });
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,4 +1,5 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
 const UserController = require('../controllers/user');
 
 router.post('/register', UserController.register);
